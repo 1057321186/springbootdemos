7b3d49e11e2194de6cbcd557c91766ec578e4bf1
renxianglong <1057321186@qq.com>
2020-10-29 22:14:38 +0800

    d

---
+++ aliossdemo/src/test/java/com/oss/ossdemo/OssdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oss.ossdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OssdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
